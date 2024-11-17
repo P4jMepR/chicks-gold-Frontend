9af64edd11a86c984430892871805d82625df501
P4jMepR <borysbronarski@gmail.com>
2024-11-17 21:17:30 +0100

    Background CORS fix

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import Header from './components/Header/Header';
 import MarketGrid from './components/MarketGrid/MarketGrid';
 import Footer from './components/Footer/Footer';
 import './styles/App.css';
+import backgroundImage from './internal-asset/background.jpg';
 
 function App() {
   return (
     <div className="app">
-      <div className="background-image" />
+      <div 
+        className="background-image" 
+        style={{ backgroundImage: `url(${backgroundImage})` }}
+      />
       <Header />
       <main className="main-content">
         <MarketGrid />
